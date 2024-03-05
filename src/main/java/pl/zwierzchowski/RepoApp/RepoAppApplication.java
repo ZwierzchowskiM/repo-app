9b04a223fa793ba3f816ed4b3e1d1ac4a1db46d1
ZwierzchowskiM <marcin.zwierzchowski1@gmail.com>
2024-03-05 12:01:39 +0100

    upload project

---
+++ src/main/java/pl/zwierzchowski/RepoApp/RepoAppApplication.java
@@ -0,0 +1,13 @@
+package pl.zwierzchowski.RepoApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/zwierzchowski/RepoApp/RepoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zwierzchowski.RepoApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
